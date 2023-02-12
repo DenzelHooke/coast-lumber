96e11a9fd9ce8c1c713f414f67069a3dcfc5bb50
Denzel Hooke <denzelhooke@hotmail.com>
2023-02-12 12:52:46 -0800

    Styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,8 @@
 const Services = () => {
   return (
-    <div className="services-index section">
+    <div className="services-index section dark-black-bg">
       <div className="global-container">
-        <h2 className="reg-text heading">SERVICES</h2>
+        <h2 className="reg-text heading white-text">SERVICES</h2>
         <ul className="services">
           <li className="services-item item-1">DELIVERY</li>
           <li className="services-item item-2">PICKUP</li>
