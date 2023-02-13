55fa8c40326d1896be42f7ac40675a00d6d41ba7
Denzel Hooke <denzelhooke@hotmail.com>
2023-02-12 21:41:33 -0800

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 const Navbar = () => {
+  const { isMobile, setIsMobile } = useState(false);
+
   return (
-    <div id="navbar" className="">
-      <div className="logo-contrainer">
-        <Image
-          src="/coast_lumber_logo.png"
-          className="small-logo"
-          alt="logo"
-          width={150}
-          height={100}
-        />
+    <>
+      <div className="nav-mobile-btn">X</div>
+      <div id="navbar" className="">
+        <div className="logo-contrainer">
+          <Image
+            src="/coast_lumber_logo.png"
+            className="small-logo"
+            alt="logo"
+            width={150}
+            height={100}
+          />
+        </div>
+        <ul className="navigation-links">
+          <li>
+            <Link href="/">HOME</Link>
+          </li>
+          <li>
+            <Link href="/about">ABOUT</Link>
+          </li>
+          <li>
+            <Link href="#">SERVICES</Link>
+          </li>
+          <li>
+            <Link href="#">CONTACT</Link>
+          </li>
+        </ul>
       </div>
-      <ul className="navigation-links">
-        <li>
-          <Link href="/">HOME</Link>
-        </li>
-        <li>
-          <Link href="/about">ABOUT</Link>
-        </li>
-        <li>
-          <Link href="#">SERVICES</Link>
-        </li>
-        <li>
-          <Link href="#">CONTACT</Link>
-        </li>
-      </ul>
-    </div>
+    </>
   );
 };
 
