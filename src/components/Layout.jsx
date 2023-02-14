3a268b293a1b6c599f617eafdd3c9b5ebbd5a925
Denzel Hooke <denzelhooke@hotmail.com>
2023-02-13 22:22:30 -0800

    Added comment in html source tag

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,11 +6,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="layout">
+        <div id="comment">Created with love by Denzel Hooke</div>
         <Navbar />
         {children}
-        <Footer />
+        <Footer />/
       </div>
-      {/* Created with love by Denzel Hooke */}
     </>
   );
 };
